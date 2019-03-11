46ae62ab6dd7b718223b78aad496c1f6454c3841
Andreas Heck <aheck@gmx.de>
2019-03-11 23:35:25 +0100

    Makes SSH port configurable

--- testbench/ssh.py
+++ testbench/ssh.py
@@ -11,7 +11,10 @@ def connect_ssh(hostname, ssh_config):
     ssh.load_system_host_keys()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-    ssh.connect(hostname, username=ssh_config['username'],
-            password=ssh_config['password'], timeout=120)
+    ssh.connect(hostname,
+            username=ssh_config['username'],
+            password=ssh_config['password'],
+            port=ssh_config['port'],
+            timeout=120)
 
     return ssh
