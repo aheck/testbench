589dbf0e278bde197f30c7281c8833d23239ae1e
Andreas Heck <aheck@gmx.de>
2019-03-12 23:58:21 +0100

    Adds key-based SSH authentication

--- testbench/ssh.py
+++ testbench/ssh.py
@@ -1,6 +1,8 @@
 import json
 import paramiko
 
+CONNECT_TIMEOUT = 360
+
 def read_ssh_config(filename):
     with open(filename, "r") as f: data=f.read()
     json_data = json.loads(data)
@@ -11,10 +13,21 @@ def connect_ssh(hostname, ssh_config):
     ssh.load_system_host_keys()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-    ssh.connect(hostname,
-            username=ssh_config['username'],
-            password=ssh_config['password'],
-            port=ssh_config['port'],
-            timeout=120)
+    port = 22
+    if 'port' in ssh_config:
+        port = ssh_config['port']
+
+    if 'keyfile' in ssh_config:
+        ssh.connect(hostname,
+                username=ssh_config['username'],
+                key_filename=ssh_config['keyfile'],
+                port=port,
+                timeout=CONNECT_TIMEOUT)
+    else:
+        ssh.connect(hostname,
+                username=ssh_config['username'],
+                password=ssh_config['password'],
+                port=port,
+                timeout=CONNECT_TIMEOUT)
 
     return ssh
