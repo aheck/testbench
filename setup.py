91d07182923e05e6936bf69baee3e036a52ca847
Andreas Heck <aheck@gmx.de>
2019-03-19 23:27:02 +0100

    Bumps version number to 0.1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='testbench',
-      version='0.1',
+      version='0.1.1',
       description='Integration Testing with Python and VirtualBox ',
       url='https://github.com/aheck/testbench',
       author='Andreas Heck',
